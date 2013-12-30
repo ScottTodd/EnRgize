dff31d55462341a922c89b344960fd284840fd04
Scott Todd <scoo531@gmail.com>
2013-12-30 12:38:49 -0500

    Testing sprites and 2D physics.

---
+++ Assets/EnRgize/Scripts/BallController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour
+{
+    private Rigidbody2D ballRigidBody;
+
+    void Start() {
+        ballRigidBody = (Rigidbody2D) transform.GetComponent<Rigidbody2D>();
+        ballRigidBody.AddForce(new Vector2(200, 200));
+    }
+
+    void Update() {
+
+    }
+}
