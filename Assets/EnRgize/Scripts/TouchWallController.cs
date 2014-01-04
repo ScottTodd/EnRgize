04723b229f1779077cf22f1a7603781d1c267a83
Scott Todd <scoo531@gmail.com>
2014-01-04 13:51:54 -0500

    Added groundwork for additional control over control/input method

--- Assets/EnRgize/Scripts/TouchWallController.cs
+++ Assets/EnRgize/Scripts/TouchWallController.cs
@@ -3,68 +3,112 @@ using System.Collections;
 
 public class TouchWallController : MonoBehaviour
 {
-    public GameObject wallObject;
+    public GameObject wallObject; // Wall with physics properties
+
+    // public so these can be observed in the inspector, not for designer editting
+    public Vector3 activeWorldPosition1;
+    public Vector3 activeWorldPosition2;
+    public Vector3 currentWorldPosition1;
+    public Vector3 currentWorldPosition2;
 
     private LightningLine lightningLineScript;
-    private Vector3 worldPosition1;
-    private Vector3 worldPosition2;
-    private Vector3 centerPosition;
-    private Vector3 differencePosition;
-    private float angle;
-    private float sign;
-    private float distance;
+    private bool inputDetected;
+    private float latestInputTime;
 
     void Start() {
         lightningLineScript = (LightningLine) transform.GetComponent<LightningLine>();
     }
+    
+    void EnableWall() {
+        wallObject.SetActive(true);
+        lightningLineScript.insideParentObject.SetActive(true);
+    }
+    
+    void DisableWall() {
+        wallObject.SetActive(false);
+        lightningLineScript.insideParentObject.SetActive(false);
+    }
 
-    void Update() {
+    // Returns true if input was detected and current positions were updated, false otherwise
+    void QueryCurrentPositions() {
+        inputDetected = false;
+
         // Attempt to get two world positions, input source dependent on platform
-#if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBPLAYER
+        #if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBPLAYER
         if (Input.GetMouseButtonDown(0)) {
+            inputDetected = true;
+            latestInputTime = Time.time;
+
             Vector2 mousePosition = Input.mousePosition;
-            worldPosition1 = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, 0));
-            worldPosition1.z = 0;
+            currentWorldPosition1 = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, 0));
+            currentWorldPosition1.z = 0;
             
-            worldPosition2 = new Vector3(-4, 2, 0);
+            currentWorldPosition2 = new Vector3(-4, 2, 0);
         }
-#else
+        #else
         if (Input.touchCount >= 2) {
+            inputDetected = true;
+            latestInputTime = Time.time;
+
             Vector2 touchPosition1 = Input.GetTouch(0).position;
-            worldPosition1 = Camera.main.ScreenToWorldPoint(new Vector3(touchPosition1.x, touchPosition1.y, 0));
-            worldPosition1.z = 0;
+            currentWorldPosition1 = Camera.main.ScreenToWorldPoint(new Vector3(touchPosition1.x, touchPosition1.y, 0));
+            currentWorldPosition1.z = 0;
             
             Vector2 touchPosition2 = Input.GetTouch(1).position;
-            worldPosition2 = Camera.main.ScreenToWorldPoint(new Vector3(touchPosition2.x, touchPosition2.y, 0));
-            worldPosition2.z = 0;
-        } else {
-            // Move offscreen
-            worldPosition1 = new Vector3(-100, 0, 0);
-            worldPosition2 = new Vector3(-100, 0, 0);
+            currentWorldPosition2 = Camera.main.ScreenToWorldPoint(new Vector3(touchPosition2.x, touchPosition2.y, 0));
+            currentWorldPosition2.z = 0;
         }
-#endif
+        #endif
+    }
 
+    void ApplyActivePositions() {
         // Set position as the center of the two world positions
-        centerPosition = (worldPosition1 + worldPosition2) / 2;
+        Vector3 centerPosition = (activeWorldPosition1 + activeWorldPosition2) / 2;
         transform.localPosition = centerPosition;
-
+        
         // Fix position of wallObject, despite physics interactions
         wallObject.transform.localPosition = new Vector3(0,0,0);
-
+        
         // Set angle as the angle from the first world position to the second
-        differencePosition = worldPosition2 - worldPosition1;
-        angle = Vector3.Angle(Vector3.right, differencePosition);
-        sign = (Vector3.Dot(Vector3.up, differencePosition) > 0.0f) ? 1.0f: -1.0f;
+        Vector3 differencePosition = activeWorldPosition2 - activeWorldPosition1;
+        float angle = Vector3.Angle(Vector3.right, differencePosition);
+        float sign = (Vector3.Dot(Vector3.up, differencePosition) > 0.0f) ? 1.0f: -1.0f;
         transform.localEulerAngles = new Vector3(0, 0, angle * sign);
-
+        
         // Scale wallObject by the distance between the two world positions
-        distance = Vector3.Distance(worldPosition1, worldPosition2);
+        float distance = Vector3.Distance(activeWorldPosition1, activeWorldPosition2);
         Vector3 localScaleCopy = wallObject.transform.localScale;
         localScaleCopy.x = distance;
         wallObject.transform.localScale = localScaleCopy;
-
+        
         // Scale lightningLine by the distance as well
         lightningLineScript.startPosition.x = -distance / 2.0f;
         lightningLineScript.endPosition.x   =  distance / 2.0f;
+    }
+
+    void Update() {
+        QueryCurrentPositions();
+        
+        if (Input.GetMouseButtonDown(0)) {
+            EnableWall();
+        }
+        if (Input.GetMouseButtonDown(1)) {
+            DisableWall();
+        }
+
+        if (inputDetected) {
+            EnableWall();
+
+            activeWorldPosition1 = currentWorldPosition1;
+            activeWorldPosition2 = currentWorldPosition2;
+            ApplyActivePositions();
+        } else {
+            #if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBPLAYER
+            
+            #else
+            DisableWall();
+            #endif
+
+        }
     }
 }
