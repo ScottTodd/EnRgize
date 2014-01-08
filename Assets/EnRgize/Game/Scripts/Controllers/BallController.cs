51e40a509d8a0ebfa6da751800015273b23452e8
Scott Todd <scoo531@gmail.com>
2014-01-08 14:23:58 -0500

    Added charge to ball, removed lightningball prefab for now

--- Assets/EnRgize/Game/Scripts/Controllers/BallController.cs
+++ Assets/EnRgize/Game/Scripts/Controllers/BallController.cs
@@ -3,14 +3,41 @@ using System.Collections;
 
 public class BallController : MonoBehaviour
 {
+    public bool charged = false;
+
     private Rigidbody2D ballRigidBody;
+    private LightningBall lightningBallScript;
+    private GameObject lightningBallObject;
 
     void Start() {
         ballRigidBody = (Rigidbody2D) transform.GetComponent<Rigidbody2D>();
         ballRigidBody.AddForce(new Vector2(200, 200));
+
+        lightningBallScript = transform.GetComponentInChildren<LightningBall>();
+        lightningBallObject = lightningBallScript.gameObject;
+
+        if (!charged) {
+            lightningBallObject.SetActive(false);
+        }
     }
 
     void Update() {
+        if (charged) {
+            lightningBallObject.SetActive(true);
+        } else {
+            lightningBallObject.SetActive(false);
+        }
 
+    }
+    
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "GroundedWall") {
+            charged = false;
+            lightningBallObject.SetActive(false);
+        }
+        if (collision.gameObject.tag == "ChargedWall") {
+            charged = true;
+            lightningBallObject.SetActive(true);
+        }
     }
 }
